5aeee420cc90c7474171591450420258a9ae1582
Student <student@MAKERSs-MacBook-Pro.local>
2019-06-27 16:56:20 +0100

    thermostat complete

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -12,14 +12,80 @@ describe('Thermostat', function() {
     expect(thermostat.currentTemp()).toEqual(20);
   });
 
-  it('increases temperature with up()', function() {
+  it('increases temperature with up', function() {
     thermostat.up();
     expect(thermostat.currentTemp()).toEqual(21);
   });
 
-  it('decreases temperature with down()', function() {
+  it('decreases temperature with down', function() {
     thermostat.down();
     expect(thermostat.currentTemp()).toEqual(19);
   });
+
+  it('minimum temperature is 10 degrees', function() {
+  for (var i = 0; i < 11; i++) {
+    thermostat.down();
+  }
+    expect(thermostat.currentTemp()).toEqual(10);
+  });
+
+  it('turns power saving mode on by default', function() {
+expect(thermostat.isPowerSavingOn()).toBe(true);
+});
+
+it('turns power saving mode off', function() {
+  thermostat.switchPowerSavingModeOff();
+expect(thermostat.isPowerSavingOn()).toBe(false);
+});
+
+it('turns power saving mode on', function() {
+  thermostat.switchPowerSavingModeOff();
+expect(thermostat.isPowerSavingOn()).toBe(false);
+thermostat.switchPowerSavingModeOn();
+expect(thermostat.isPowerSavingOn()).toBe(true);
+});
+
+it('power saving mode maximum set to 25 degrees', function() {
+for (var i = 0; i < 6; i++) {
+  thermostat.up();
+}
+  expect(thermostat.currentTemp()).toEqual(25);
+});
+
+it('power saving mode maximum when off set to 32 degrees', function() {
+  thermostat.switchPowerSavingModeOff();
+for (var i = 0; i < 13; i++) {
+  thermostat.up();
+}
+  expect(thermostat.currentTemp()).toEqual(32);
+});
+
+it('reset temperature to 20 degrees', function() {
+for (var i = 0; i < 3; i++) {
+  thermostat.up();
+}
+thermostat.resetTemp();
+  expect(thermostat.currentTemp()).toEqual(20);
+});
+
+it('low energy usage', function() {
+for (var i = 0; i < 3; i++) {
+  thermostat.down();
+}
+  expect(thermostat.energyUsage()).toEqual('low-usage');
+});
+
+it('medium energy usage', function() {
+  expect(thermostat.energyUsage()).toEqual('medium-usage');
+  });
+
+  it('high energy usage', function() {
+    thermostat.switchPowerSavingModeOff();
+  for (var i = 0; i < 6; i++) {
+    thermostat.up();
+  }
+    expect(thermostat.energyUsage()).toEqual('high-usage');
+  });
+
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,20 +1,69 @@
 'use strict';
 
 function Thermostat() {
-   this.temp = 20;
-
-   }
+this.MAX_LOW_ENERGY_USAGE = 18
+this.MINIMUM_TEMP = 10
+this.powerSavingMode = true
+this.MAX_PSM_ON = 25
+this.MAX_PSM_OFF = 32
+this.DEFAULT_TEMP = 20
+this.temp = this.DEFAULT_TEMP;
+};
 
 Thermostat.prototype.currentTemp = function() {
   return this.temp;
   };
 
 Thermostat.prototype.up = function() {
+  if (this.isMAXIMUM_TEMP()) {
+    return;
+  }
     this.temp += 1;
 
-  }
+  };
 
 Thermostat.prototype.down = function() {
+  if (this.isMINIMUM_TEMP()) {
+    return;
+  };
       this.temp -= 1;
+  };
 
-  }
+  Thermostat.prototype.isMINIMUM_TEMP = function() {
+        return this.temp === this.MINIMUM_TEMP;
+
+    };
+
+    Thermostat.prototype.isMAXIMUM_TEMP = function() {
+      if (this.isPowerSavingOn()) {
+        return this.temp === this.MAX_PSM_ON;
+      };
+        return this.temp === this.MAX_PSM_OFF;
+
+      };
+
+    Thermostat.prototype.isPowerSavingOn = function() {
+       return this.powerSavingMode === true;
+      };
+
+      Thermostat.prototype.switchPowerSavingModeOff = function() {
+         this.powerSavingMode = false;
+        };
+
+        Thermostat.prototype.switchPowerSavingModeOn = function() {
+           this.powerSavingMode = true;
+          };
+
+          Thermostat.prototype.resetTemp = function() {
+             this.temp = this.DEFAULT_TEMP;
+            };
+
+            Thermostat.prototype.energyUsage = function() {
+               if (this.temp < this.MAX_LOW_ENERGY_USAGE) {
+                 return 'low-usage';
+               } else if (this.temp < this.MAX_PSM_ON) {
+                 return 'medium-usage';
+               };
+               return 'high-usage';
+
+              };
